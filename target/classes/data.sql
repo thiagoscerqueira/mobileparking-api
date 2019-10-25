a7bf767644c04b7aca02788688f833dcb808c969
Thiago Cerqueira <thiagoscerqueira@gmail.com>
2019-10-24 23:09:38 -0300

    Criando repo

---
+++ target/classes/data.sql
@@ -0,0 +1,9 @@
+insert into usuario(id, nome, saldo_atualizado) values ('thiago', 'Thiago Cerqueira', 0);
+insert into usuario(id, nome, saldo_atualizado) values ('rafael', 'Rafael', 0);
+insert into usuario(id, nome, saldo_atualizado) values ('phelipe', 'Phelipe', 0);
+
+insert into tempo_permanencia (tempo, valor) values (15, 1);
+insert into tempo_permanencia (tempo, valor) values (30, 2);
+insert into tempo_permanencia (tempo, valor) values (60, 4);
+insert into tempo_permanencia (tempo, valor) values (120, 7);
+insert into tempo_permanencia (tempo, valor) values (240, 10);
